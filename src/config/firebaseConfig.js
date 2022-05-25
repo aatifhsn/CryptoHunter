9dcc44bc6f11d53e4f7937661e29481c985f120c
aatif-op <80807598+aatif-op@users.noreply.github.com>
2022-05-25 12:37:32 +0530

    commit message

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCf-TZFCEMkKh--ok2Y-qcST4k6V9yiPA4",
+    authDomain: "cryptohunter-b9af2.firebaseapp.com",
+    projectId: "cryptohunter-b9af2",
+    storageBucket: "cryptohunter-b9af2.appspot.com",
+    messagingSenderId: "144006331167",
+    appId: "1:144006331167:web:dab24731fb8a89eb64ce2a"
+};
+
+export default firebaseConfig;
